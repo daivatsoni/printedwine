af8563c1cb4c9250ccffb9ac794c5f9134c74b38
Daivat Soni <daivatsoni@gmail.com>
2016-11-04 16:39:00 +0530

    WooCommerce Plugin Upgrade
    Updated woocommerce plugin for better layout options.

--- wp-content/themes/wine-child/functions.php
+++ wp-content/themes/wine-child/functions.php
@@ -395,4 +395,14 @@ function product_slider( $atts ) {
 
 }
 
-?>
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
+            padding-bottom: 30px;
+            background-size: 200px;
+            width: 200px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
