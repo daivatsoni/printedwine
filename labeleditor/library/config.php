c146b5bdce6d690f0de4cd814975d8adbaa8e8f2
Frederic <kpomservices@gmail.com>
2017-01-10 17:29:04 +0530

    updated config

--- labeleditor/library/config.php
+++ labeleditor/library/config.php
@@ -3,9 +3,13 @@
 	error_reporting(E_ALL);
 
 	$mysql_ip = 'localhost';
-	$mysql_user = 'daiva1l1_kpom';
-	$mysql_pass = '2f?37ziv1g7B+t@HRA';
-	$mysql_db = 'daiva1l1_kpom';
+	$mysql_user = 'demourl_pwine';
+	$mysql_pass = 'm!fmw7pHb*{L';
+	$mysql_db = 'demourl_pwine';
+
+	//$mysql_user = 'daiva1l1_kpom';
+	//$mysql_pass = '2f?37ziv1g7B+t@HRA';
+	//$mysql_db = 'daiva1l1_kpom';
 
 	$conn = mysql_connect ($mysql_ip, $mysql_user, $mysql_pass) or die ("I cannot connect to the database because: " . mysql_error());
 	mysql_select_db ($mysql_db) or die ("I cannot select the database '$dbname' because: " . mysql_error());
