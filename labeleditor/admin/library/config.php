968fb2209b3688bf5e85fbdd399eb0ef7855d1a3
unknown <kpomservices@gmail.com>
2017-01-30 12:02:34 +0530

    updated gitignore

--- labeleditor/admin/library/config.php
+++ labeleditor/admin/library/config.php
@@ -4,8 +4,14 @@
 	$mysql_pass = '2f?37ziv1g7B+t@HRA';
 	$mysql_db = 'daiva1l1_kpom';
 
+	// Report simple running errors
+	error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+	//$mysql_user = 'root';
+	//$mysql_pass = '';
+	//$mysql_db = 'labeleditor';
 	//$item_per_page = 20;
-	
+
 	$conn = mysql_connect ($mysql_ip, $mysql_user, $mysql_pass) or die ("I cannot connect to the database because: " . mysql_error());
 	mysql_select_db ($mysql_db) or die ("I cannot select the database '$dbname' because: " . mysql_error());
 	date_default_timezone_set('GMT');
